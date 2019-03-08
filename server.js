53147aca88badf782141dc1685fb266168aa3e68
cstra <cooperstrahan@gmail.com>
2019-03-07 17:35:20 -0800

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-// app.get('/', function(request, response) {
-// })
+
 app.listen(port, function() {
 })
 app.use(express.static(__dirname + "/static"));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+    response.render('index');
+})
+
+app.get('/cars', function(request, response) {
+    response.render('cars');
+})
+
+app.get('/cats', function(request, response) {
+    response.render('cats');
+})
+
+app.get('/cars/new', function(request, response) {
+    response.render('form');
+})
